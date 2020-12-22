dcc8182ebd76dadaa782848c41102b746a8da0a3
ishowshao <ishowshao@me.com>
2020-12-22 18:01:39 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,63 @@
 <template>
-  <div id="app">
-    <div class="schema">
-      <el-input type="textarea" :rows="20" placeholder="请输入内容" v-model="textarea">
-      </el-input>
-      <el-button @click="gen">生成</el-button>
+    <div id="app">
+        <div class="schema">
+            <el-input type="textarea" :rows="20" placeholder="请输入内容" v-model="textarea"> </el-input>
+            <el-button @click="gen">生成</el-button>
+        </div>
+        <div class="form">
+            <el-form ref="form" label-width="80px">
+                <object-generator ref="root" :schema="schema" :output="output"></object-generator>
+                <el-form-item>
+                    <el-button @click="generateOutput">生成output</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
+        <div class="output">output</div>
     </div>
-    <div class="form">
-      <el-form ref="form" label-width="80px">
-        <object-generator :schema="schema" :output="output"></object-generator>
-        <el-form-item>
-          <el-button @click="generateOutput">生成output</el-button>
-        </el-form-item>
-      </el-form>
-    </div>
-    <div class="output">output</div>
-  </div>
 </template>
 
 <script>
-import input from '@/assets/input.json'
-import schema2output from '@/lib/output'
-import ObjectGenerator from './components/ObjectGenerator.vue'
+import input from '@/assets/input.json';
+import schema2output from '@/lib/output';
+import ObjectGenerator from './components/ObjectGenerator.vue';
 
 export default {
-  name: "App",
-  components: {
-    ObjectGenerator
-  },
-  data() {
-    return {
-      textarea: JSON.stringify(input, null, '  '),
-      schema: {},
-      output: {},
-    }
-  },
-  methods: {
-    gen() {
-      this.schema = JSON.parse(this.textarea);
-      this.output = this.generateOutput();
+    name: 'App',
+    components: {
+        ObjectGenerator
     },
-    generateOutput() {
-      const schema = JSON.parse(this.textarea);
-      console.log(schema);
-      console.log(schema2output(schema));
-      return schema;
+    data() {
+        return {
+            textarea: JSON.stringify(input, null, '  '),
+            schema: {},
+            output: {}
+        };
+    },
+    methods: {
+        gen() {
+            this.schema = JSON.parse(this.textarea);
+        },
+        generateOutput() {
+            const value = this.$refs['root'].getValue();
+            console.log(value);
+        }
     }
-  }
 };
 </script>
 
 <style>
 body {
-  margin: 0;
+    margin: 0;
 }
 #app {
-  display: flex;
+    display: flex;
 }
 .schema {
-  width: 40vw;
-  padding: 10px;
-  box-sizing: border-box;
+    width: 40vw;
+    padding: 10px;
+    box-sizing: border-box;
 }
 .form {
-  width: 30vw;
+    width: 30vw;
 }
 </style>

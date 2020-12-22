6604adc4b71803642239b0e9603ae9b133aae5b7
ishowshao <ishowshao@me.com>
2020-12-22 16:30:01 +0800

    Update StringGenerator.vue

--- src/components/StringGenerator.vue
+++ src/components/StringGenerator.vue
@@ -1,10 +1,23 @@
 <template>
     <el-form-item :label="name" :key="name">
-        <el-input v-model="schema.value"></el-input>
+        <el-input v-model="value"></el-input>
     </el-form-item>
 </template>
 <script>
 export default {
     props: ['schema', 'name'],
+    data() {
+        return {
+            value: null,
+        };
+    },
+    created() {
+        this.value = this.schema.default || null;
+    },
+    watch: {
+        value(newValue) {
+            this.schema.value = newValue;
+        },
+    },
 }
 </script>
